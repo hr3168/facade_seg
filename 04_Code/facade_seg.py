ff1841a14084020041f842bf9c847b034269495c
hr3168 <96618282+hr3168@users.noreply.github.com>
2023-02-27 15:30:48 +0800

    init commit

---
+++ 04_Code/facade_seg.py
@@ -0,0 +1,27 @@
+from keras_segmentation.models.unet import vgg_unet
+import numpy as np 
+import pandas as pd
+
+print("test")
+
+model = vgg_unet(n_classes=256 ,  input_height=416, input_width=608  )
+
+model.train(
+    train_images =  "./content/images_prepped_train/",
+    train_annotations = "./content/annotations_prepped_train/",
+    checkpoints_path = "./tmp/vgg_unet_1", 
+    steps_per_epoch=2048,
+    auto_resume_checkpoint=True,
+    epochs=10
+)
+
+out = model.predict_segmentation(
+    inp="./content/images_prepped_train/cmp_b0025.jpg",
+    out_fname="./tmp/out.png"
+)
+
+# import matplotlib.pyplot as plt
+# plt.imshow(out)
+
+# evaluating the model 
+print(model.evaluate_segmentation( inp_images_dir="./content/images_prepped_train/"  , annotations_dir="./content/annotations_prepped_train/" ) )
