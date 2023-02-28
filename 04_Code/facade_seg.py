02bfaf2f36b0cb9494428c395101f5575dc3d034
hr3168 <96618282+hr3168@users.noreply.github.com>
2023-02-28 17:34:12 +0800

    0227

--- 04_Code/facade_seg.py
+++ 04_Code/facade_seg.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 print("test22")
 
-model = vgg_unet(n_classes=12 ,  input_height=416, input_width=608  )
+model = vgg_unet(n_classes=5 ,  input_height=416, input_width=608  )
 
 model.train(
     train_images =  "./content/images_prepped_train/",
-    train_annotations = "./content/annotations_prepped_train/",
+    train_annotations = "./content/label/",
     checkpoints_path = "./tmp/vgg_unet_2", 
     steps_per_epoch=2048,
     auto_resume_checkpoint=True,
