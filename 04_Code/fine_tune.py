22e3b6d04612428439dde4a48823cc4dc945bfa4
Riceball <chenlei9691@gmail.com>
2023-03-06 12:31:11 +0800

    Create fine_tune.py

---
+++ 04_Code/fine_tune.py
@@ -0,0 +1,32 @@
+from keras_segmentation.models.unet import vgg_unet
+import numpy as np 
+import pandas as pd
+import os
+import pdb
+from keras_segmentation.predict import model_from_checkpoint_path
+from keras_segmentation.models.unet import unet_mini
+from keras_segmentation.models.model_utils import transfer_weights
+
+arcade_seg = vgg_unet(n_classes=15 ,  input_height=416, input_width=608  )
+
+# pdb.set_trace()
+
+model_origin = model_from_checkpoint_path( "D:\Code\\facade_seg-main\\04_Code\\tmp\\origin\\vgg_unet_2" )
+
+transfer_weights( model_origin , arcade_seg  ) 
+
+# pdb.set_trace()
+
+arcade_seg.train(
+    train_images =  "./qilou_dataset/image/",
+    train_annotations = "./qilou_dataset/anote/",
+    checkpoints_path = "./qilou_seg/vgg_unet_2" , 
+    steps_per_epoch=2048,
+    auto_resume_checkpoint=True,
+    epochs=15
+)
+
+out = arcade_seg.predict_segmentation(
+    inp="./qilou_dataset/image/Snipaste_2023-02-21_12-13-00.png",
+    out_fname="./qilou_seg/out_1.png"
+)
